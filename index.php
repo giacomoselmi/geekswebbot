b4d65a183362daad335b300e54d3ae71333e1f86
giacomoselmi <gselmi@gselmi-ltm1.internal.salesforce.com>
2016-08-23 19:40:24 +0200

    modified access token
    modified access token

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
 */
 require 'vendor/autoload.php';
 
-$client = new Zelenin\Telegram\Bot\Api(''); // Set your access token
+$client = new Zelenin\Telegram\Bot\Api('191402453:AAFj4ww8v_YDa60iDi9Ck6iCMbX4izfUSLQ'); // Set your access token
 $url = ''; // URL RSS feed
 $update = json_decode(file_get_contents('php://input'));
 
