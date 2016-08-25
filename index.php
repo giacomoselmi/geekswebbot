e579644e094dbd8b3a0263d32bd4ceb158d2ce49
giacomoselmi <gselmi@gselmi-ltm1.internal.salesforce.com>
2016-08-25 16:00:51 +0200

    create case 1

--- index.php
+++ index.php
@@ -61,6 +61,30 @@ try {
         'chat_id' => $update->message->chat->id,
         'text' => "I will create a Case for you"
         ]);
+
+        //create a connection string from the PG database URL and then use it to connect
+        $url=parse_url(getenv("DATABASE_URL"));
+        $host = $url["host"];
+        $port = $url["port"];
+        $user = $url["user"];
+        $password = $url["pass"];
+        $dbname = substr($url["path"],1);
+        $connect_string = "host='" . $host . "' ";
+        $connect_string = $connect_string . "port=" . $port . " ";
+        $connect_string = $connect_string . "user='" . $user . "' ";
+        $connect_string = $connect_string . "password='" . $password . "' ";
+        $connect_string = $connect_string . "dbname='" . $dbname . "' ";
+        $db = pg_connect($connect_string);
+
+        $query = "INSERT INTO Case (AccountId, Description, Priority, RecordTypeId, Status)
+        VALUES ('0012400000eiYSb', 'A Support Case', 'Medium', '	012240000002iSCAAY', 'New');";
+        $result = pg_query($query) or die('Query failed: ' . pg_last_error());
+
+        // free resultset
+        pg_free_result($result);
+        // close connection
+        pg_close($db);
+
     }
     // else
     // {
